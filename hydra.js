70b2620a492d53451c435a2a0ebc66755a824564
ndr0n <esquilofreniko@gmail.com>
2024-02-25 00:28:39 +0000

    p5 Clean

---
+++ hydra.js
@@ -0,0 +1,15 @@
+// create a new hydra-synth instance
+var hydra = new Hydra({ detectAudio: false, });
+// osc(4, 0.1, 1.2).out();
+
+
+
+// p5 = new P5(width, height, 'P2D');
+// p5.rect(300, 100, 100, 100);
+// s0.init({src: p5.canvas});
+// p5.show();
+// src(s0).repeat().out()
+
+osc().out();
+
+
